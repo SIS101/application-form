01d3c03d43458aca5d7bf3cd52c09247172efd7f
fshangala <fshangala@gmail.com>
2021-09-15 11:50:35 +0200

    no noticeable fatal errors ready for launch

--- src/components/custom_modules/api_config.js
+++ src/components/custom_modules/api_config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://192.168.0.111/public-api/public/api';
-export const serverUrl = 'http://192.168.0.111/public-api/public';
+export const baseUrl = 'http://localhost:8000/api';
+export const serverUrl = 'http://localhost:8000';
--- src/components/views/FrontPage.jsx
+++ src/components/views/FrontPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { baseUrl } from "../custom_modules/api_config";
+import done from '../../done.gif';
 
 class FrontPage extends React.Component {
     constructor(props){
@@ -42,19 +43,28 @@ class FrontPage extends React.Component {
 
     logout(){
         sessionStorage.removeItem('api_token');
+        this.auth = sessionStorage.getItem('api_token') || null;
         this.props.history.push('/');
     }
 
     render(){
-        console.log(this.application);
         if(this.auth){
             if(this.application){
                 if(this.application.success){
                     if(this.application.data.application_submitted){
                         return (
-                            <div className="collection">
-                                <Link to="/application/status" className="collection-item">Application Status</Link>
-                                <Link to="#" onClick={this.logout} className="collection-item">Logout</Link>
+                            <div className="card indigo darken-4 white-text" style={{margin: "2em"}}>
+                                <div className="card-image">
+                                    <img src={done} alt="done" />
+                                    <span className="card-title black-text">Application Status</span>
+                                </div>
+                                <div className="card-content">
+                                    <p className="flow-text">Your application is submitted, you will be notified by means of email when your application is reviewed.</p>
+                                    <p className="flow-text">Frequently check your email inbox.</p>
+                                </div>
+                                <div className="card-action">
+                                    <Link to="#" onClick={this.logout}>Logout</Link>
+                                </div>
                             </div>
                         );
                     } else {
@@ -72,11 +82,10 @@ class FrontPage extends React.Component {
                         );
                     }
                 } else {
-                    alert(this.application.message);
                     return (
                         <div className="collection">
-                            <Link to="/login" className="collection-item">Login</Link>
-                            <Link to="/initialization" className="collection-item">New Online Application</Link>
+                            <Link to="/page/1" className="collection-item">Personal Information</Link>
+                            <Link to="#" onClick={this.logout} className="collection-item">Logout</Link>
                         </div>
                     );
                 }
